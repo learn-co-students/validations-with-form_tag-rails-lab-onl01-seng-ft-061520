40405f9035a6f5cd413a6d871c0a75708c9993ac
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-08-26 10:40:09 -0700

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -20,6 +20,15 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def update
+    set_author
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def author_params
