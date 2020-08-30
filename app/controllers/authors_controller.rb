d630313cba760fcd66fb8f51faf605d30ad29880
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-08-30 15:11:59 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,11 +8,20 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    @author.save ?
+    redirect_to(author_path(@author)) : render(:new)
+    # if @author.save
+    #   redirect_to author_path(@author)
+    # else
+    #   render :new
+    # end
   end
 
+  def index
+    @authors = Author.all
+  end
+ 
   private
 
   def author_params
